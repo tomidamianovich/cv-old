e60822cad6db4edbfa269282e5da1629043996e8
tomas damianovich <tomasdamianovichreddy@github.com>
2021-05-03 15:38:31 -0300

    adding proxy and heroku

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
 const APP_NAME = "CV";
-const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "/";
 const BASE_URL_API = `${BASE_URL}api/`;
 const BASE_URL_API_PATHS = {
   PERSONAL_DATA: "person",
