e791273b9e350438d9c890aa55abb9ee2274b81c
tomas damianovich <tomasdamianovichreddy@github.com>
2021-05-03 19:24:18 -0300

    adding fix ion procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node app.js
+web: node backend/src/app.js
--- backend/src/app.js
+++ backend/src/app.js
@@ -6,13 +6,13 @@ const app = express();
 // settings
 app.set('port', process.env.PORT || 8000);
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('../../frontend/build'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../../frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 // middlewares
 app.use(cors());
